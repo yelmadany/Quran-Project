cd3216c997a26ad040438a92bb97d98af2ae593d
Youssef <yelmadany2@gmail.com>
2024-11-16 23:21:07 +0400

    Test

---
+++ main.py
@@ -0,0 +1,15 @@
+#Libraries
+import os
+from pydub import AudioSegment
+
+#Variables
+numOfCRs = input() #Number of Continous Recitations
+
+#Data Setup
+folder_path = 'Data'
+file_list = os.listdir(folder_path)
+quranDict = {str(i).zfill(3): [] for i in range(1, 115)}
+for file in file_list:
+    chapter = file[0:3]
+    quranDict[chapter].append(file)
+
